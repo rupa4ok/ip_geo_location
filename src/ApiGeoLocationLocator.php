3914a2a972e8ced630979a1c944e09617ff2f6d4
Rupa4ok <rupak4@yandex.ru>
2019-12-28 16:28:06 +0300

    Add locator interface

---
+++ src/ApiGeoLocationLocator.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class ApiGeoLocationLocator implements Locator
+{
+    private $client;
+    private $apiKey;
+    
+    public function __construct(HttpClient $client, string $apiKey = '6b19f7a24fa749d183617586a80dd462')
+    {
+        $this->client = $client;
+        $this->apiKey = $apiKey;
+    }
+    
+    public function locate(Ip $ip): ?Location
+    {
+        $url = 'https://api.ipgeolocation.io/ipgeo?' . http_build_query([
+                'apiKey' => $this->apiKey,
+                'ip' => $ip->getValue()
+            ]);
+        
+        $response = $this->client->get($url);
+        $data = json_decode($response, true);
+        $data = array_map(function ($value) {
+            return $value !== '-' ? $value : null;
+        }, $data);
+        
+        if (empty($data['country_name'])) {
+            return null;
+        }
+        
+        return new Location(
+            $data['country_name'],
+            $data['state_prov'],
+            $data['city']
+        );
+    }
+}
--- src/Locator.php
+++ src/Locator.php
@@ -4,38 +4,7 @@ declare(strict_types=1);
 
 namespace App;
 
-class Locator
+interface Locator
 {
-    private $client;
-    private $apiKey;
-    
-    public function __construct(HttpClient $client, string $apiKey = '6b19f7a24fa749d183617586a80dd462')
-    {
-        $this->client = $client;
-        $this->apiKey = $apiKey;
-    }
-    
-    public function locate(Ip $ip): ?Location
-    {
-        $url = 'https://api.ipgeolocation.io/ipgeo?' . http_build_query([
-                'apiKey' => $this->apiKey,
-                'ip' => $ip->getValue()
-            ]);
-        
-        $response = $this->client->get($url);
-        $data = json_decode($response, true);
-        $data = array_map(function ($value) {
-            return $value !== '-' ? $value : null;
-        }, $data);
-        
-        if (empty($data['country_name'])) {
-            return null;
-        }
-        
-        return new Location(
-            $data['country_name'],
-            $data['state_prov'],
-            $data['city']
-        );
-    }
+    public function locate(Ip $ip): ?Location;
 }
